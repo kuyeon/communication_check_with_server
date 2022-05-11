12dd211bcd258a810098142c992eb095bc24a0bd
Park Kuyeon <53827090+kuyeon@users.noreply.github.com>
2022-05-11 21:02:41 +0900

    Update communication_check_with_server.sh

--- communication_check_with_server.sh
+++ communication_check_with_server.sh
@@ -2,10 +2,10 @@
 
 server_ip=192.168.1.1 # This is an example, change this to your server ip
 server_status=1
-while [ $server_status -eq 1 ]
+while [ $server_status -eq 1 ]    # "-eq" is a comparison operator. it is used when comparing integers.
 do
-        ping -c 1 -w 1 $server_ip > /dev/null
-        if [ $? -eq 0 ] ; then
+        ping -c 1 -w 1 $server_ip > /dev/null    # ping once, if not work wait 1 second. ("> /dev/null" means clearing the command output.)
+        if [ $? -eq 0 ] ; then    # $? means the return value of the previously executed command 
                 echo "Communication Success"
                 server_status=0
         else
